8ce69ca70cf0b706ab94f8ff01953f49457f46cd
Zaka Farzali <zeka.farzali@gmail.com>
2021-02-22 19:10:06 +0100

    pre-deployment changes

--- portfolioapp/views.py
+++ portfolioapp/views.py
@@ -14,10 +14,12 @@ def home(request):
         message_email = request.POST['email']
         message_phone = request.POST['phone']
         message = request.POST['message']
+        if message_name =='':
+            return render(request,'home.html',{'project': project, 'error': 'Please enter your name'})
         if not (re.search(regex,message_email)):
-            return render(request,'home.html',{'project': project, 'error': 'Please enter valid email.'})
+            return render(request,'home.html',{'project': project, 'error': 'Please enter valid email'})
         if message =='':
-            return render(request,'home.html',{ 'project': project, 'error': 'Please fill the message box.'})
+            return render(request,'home.html',{ 'project': project, 'error': 'Please fill the message box'})
         send_mail(
             subject = 'Message from '+message_name,
             message = message + '\n' + message_email +'\n'+ message_phone ,
@@ -28,3 +30,4 @@ def home(request):
         return render(request,'home.html',{'project': project, 'notification': 'Dear '+ message_name})
     else:
         return render(request,'home.html',{'project': project})
+
